c0478ce54c483f045c36190515dc088683c45031
bolaji ogidan <bolajiscott@yahoo.com>
2023-02-28 16:09:00 +0100

    components-created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <h1 className="text-3xl font-bold underline">Hello w</h1>
     </div>
   );
 }
